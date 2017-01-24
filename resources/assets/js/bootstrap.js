6e8135a7edb7b59a8d09566ad048e1aa4f4c6183
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2017-01-24 20:25:32 -0300

    upgrade to Laravel 5.4

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -2,6 +2,12 @@
 window._ = require('lodash');
 
 /**
+ * Parse, validate, manipulate, and display dates in JavaScript.
+ */
+
+window.moment = require('moment');
+
+/**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
@@ -9,11 +15,7 @@ window._ = require('lodash');
 
 window.$ = window.jQuery = require('jquery');
 
-/**
- * Parse, validate, manipulate, and display dates in JavaScript.
- */
-
-window.moment = require('moment');
+require('bootstrap-sass');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
@@ -23,57 +25,30 @@ window.moment = require('moment');
 
 window.Vue = require('vue');
 
-
 /**
- * The plugin for Vue.js provides services for making web requests
- * and handle responses using a XMLHttpRequest or JSONP.
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-require('vue-resource');
+window.axios = require('axios');
 
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest'
+};
 
 /**
- *  Events is a var that has a vue instance for haddle events such as emit, on, off
- *
- *  to emit   -> Events.$emit('key', val);
- *  to listen -> Events.$on('key', function(val){ console.log(val) });
- *  to off    -> Events.$off('key', val);
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
  */
 
-window.Events = new Vue({});
+// import Echo from "laravel-echo"
 
-/**
- * We'll register a HTTP interceptor to attach the "CSRF" header to each of
- * the outgoing requests issued by this application. The CSRF middleware
- * included with Laravel will automatically verify the header's value.
- */
-
-Vue.http.interceptors.push((request, next) => {
-  request.headers['X-CSRF-TOKEN'] = Laravel.csrfToken;
-
-  next();
-});
-
-
-/**
-* Laravel routes
-*/
-
-window.Routes = require('./routes.js');
-
-
-/**
-* Config
-*/
-
-require('./config');
-
-
-/**
-* Filters
-*/
-
-require('./filters');
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'your-pusher-key'
+// });
 
 /**
 * Semantic UI
